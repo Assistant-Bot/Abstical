069ddba4a09075fe858898db04e26b79bdb5460b
Olybear9 <olybear9@gmail.com>
2021-02-10 13:49:53 -0600

    📝 Add exports

--- mod.ts
+++ mod.ts
@@ -13,3 +13,10 @@
  * as published by the Free Software Foundation; either version 3
  * of the License, or (at your option) any later version.
  */
+export { default as Client } from "./src/client/Client.ts";
+export { default as Connection } from "./src/server/Connection.ts";
+export { default as Response } from "./src/client/Response.ts";
+export { default as Request } from "./src/server/Request.ts";
+export { default as Server } from "./src/server/Server.ts";
+
+export * as OP from "./src/OP.ts";
